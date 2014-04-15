3b1aae1bb2b3577aba6da3433effe3fbaabfdd92
Peter Zeppieri <peterzeppieri@yahoo.com>
2014-04-15 00:05:29 +0000

    Modified deployment script

--- deployment/config/deploy.rb
+++ deployment/config/deploy.rb
@@ -62,7 +62,7 @@ namespace :deploy do
     run "sudo service httpd stop"
   end
 
-  desc "Restart Application"
+  desc "Restart My Application"
   task :restart, :roles => :app do
     run "sudo service httpd restart"
   end
